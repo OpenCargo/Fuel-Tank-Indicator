1e93dba348f607d5b30c33eedfba0d98505f14b0
Davide Altamura <davide.altamura97@gmail.com>
2023-11-29 18:27:13 +0100

    Working tank1 GUI

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
 import customtkinter
+from udp_listener import udp_listener as udp
+import threading
+import math
+
+DIAMETER = 1.84
+LENGHT = 1.86
+TANK1_CAPACITY = 4950
+
+def calculate_liters(data):
+    fuel_height = DIAMETER - float(data)/100
+    alfa = math.acos((DIAMETER - 2 * fuel_height) / DIAMETER)
+    liters = 1000*LENGHT * (alfa * (DIAMETER/2)**2 - (DIAMETER/2 - fuel_height) * math.sqrt(DIAMETER * fuel_height - fuel_height**2))
+    return liters
 
 
-value = 0.5
+def update_liters_and_percentage(liters_label, percentage_label, progress_bar, data):
+    liters = calculate_liters(data)
+    capacity_percentage = (liters / TANK1_CAPACITY) * 100
+
+    liters_label_text = f"Liters: {liters:.2f}"
+    liters_label.configure(text=liters_label_text)
+
+    percentage_label_text = f"{capacity_percentage:.2f}%"
+    percentage_label.configure(text=percentage_label_text)
+
+    progress_bar_value = liters / TANK1_CAPACITY
+    progress_bar.set(progress_bar_value)
+
+
+#starting values
+progressbar_value1 = 0.5
+percentage_tank1 = progressbar_value1 * 100
+liters1 = 0 
+
 app = customtkinter.CTk()
 app.geometry("600x500")
 app.title("Fuel Indicator")
 
-label = customtkinter.CTkLabel(app, text ="Tank 1:")
-label.grid(row=0,column=0, padx=40, pady=40)
-progressbar = customtkinter.CTkProgressBar(app, orientation="vertical", mode= "determinate")
-progressbar.grid(row=1, column=0, padx=40, pady=40)
-progressbar.set(value)
-label = customtkinter.CTkLabel(app, text ="%")
-label.grid(row=1,column=1, padx=40, pady=40)
-label = customtkinter.CTkLabel(app, text ="Liters:")
-label.grid(row=2,column=0, padx=40, pady=40)
-label = customtkinter.CTkLabel(app, text ="0")
-label.grid(row=2,column=1, padx=40, pady=40)
+label_tank = customtkinter.CTkLabel(app, text="Tank 1:", font=(None, 30))
+label_tank.grid(row=0, column=0, padx=40, pady=40)
+
+progressbar = customtkinter.CTkProgressBar(app, orientation="vertical", width=80, corner_radius=5)
+progressbar.grid(row=1, column=0, padx=40, pady=10)
+progressbar.set(progressbar_value1)
+
+label_percentage = customtkinter.CTkLabel(app, text=f"{percentage_tank1}%", font=(None, 20))
+label_percentage.grid(row=1, column=0, padx=40, pady=40)
+
+label_liters = customtkinter.CTkLabel(app, text=f"Liters: {liters1}", font=(None, 20))
+label_liters.grid(row=2, column=0, padx=40, pady=40)
+
+udp_thread = threading.Thread(target=udp, args=((update_liters_and_percentage, label_liters, label_percentage, progressbar),))
+udp_thread.start()
 
 app.mainloop()
+
+
+
--- udp_listener.py
+++ udp_listener.py
@@ -1,13 +1,17 @@
 import socket
 
-UDP_IP = "192.168.1.6"
-UDP_PORT = 1234
+def udp_listener(update_function_args):
+    update_function, *args = update_function_args
+    UDP_IP = "192.168.1.17"
+    UDP_PORT = 12345
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    print("Received message:", data.decode())
+    while True:
+        data, addr = sock.recvfrom(1024)
+        decoded_data = data.decode()
+        update_function(*args, decoded_data)
+
 
 
